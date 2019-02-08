23e67b1d68cff6dd6de6fe775daa35ef64d9c01e
mavcycyan <pbl6ewka@gmail.com>
2019-02-08 11:24:27 +0200

    - доделан слайдер на главной - доделана главная в соответствии с требовании - проверен адаптив

--- frames/strange-slider/strange-slider.js
+++ frames/strange-slider/strange-slider.js
@@ -1,8 +1,32 @@
 $(window).on('load', function(){
     var slPager = '';
     $('#strange-slider').find('.strange-slider-bl').each(function(id, el){
-        
-        slPager = slPager + '<li><a href="#" id="pg'+Number(id+1)+'">'+Number(id+1)+'</a></li>'
+        if(id == 0){
+            $(el).addClass('active');
+            slPager = slPager + '<li><a href="#'+Number(id+1)+'" class="active">'+Number(id+1)+'</a></li>';
+        }
+        else {
+            slPager = slPager + '<li><a href="#'+Number(id+1)+'" >'+Number(id+1)+'</a></li>';
+        }
     });
     $('#strange-slider').append('<ul class="strange-slider-pager">'+slPager+'</ul>');
+});
+var slBef;
+var slHref;
+var slThis;
+$('#strange-slider').on('click', '.strange-slider-pager li a', function(e){
+    e.preventDefault();
+    slThis = $(this);
+    slHref = $(this).attr('href').replace('#', '');
+    slBef = $('.strange-slider-bl.active');
+    slBef.removeClass('active');
+    slBef.addClass('change');
+    $('#strange-slider li a.active').removeClass('active');
+    slThis.addClass('active'); 
+    setTimeout(function(){
+        slBef.removeClass('change');
+        $('.strange-slider-bl:nth-child('+slHref+')').addClass('active');       
+    }, 1000);
+  
+
 });
